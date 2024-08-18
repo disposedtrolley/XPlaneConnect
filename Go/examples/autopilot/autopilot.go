283e2e18b2123b2bc06106454eab98f98bb4aaa4
James Liu <james@jamesliu.io>
2024-08-18 15:46:21 +1000

    go: Add a wing leveler autopilot

---
+++ Go/examples/autopilot/autopilot.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"xpc"
+
+	"go.einride.tech/pid"
+)
+
+func main() {
+	conn, err := xpc.Dial(xpc.Host{
+		XPHost:  "localhost",
+		XPPort:  49009,
+		Timeout: time.Millisecond * 50,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	samplingInterval := 100 * time.Millisecond
+
+	ticker := time.NewTicker(samplingInterval)
+
+	P := 0.05
+	I := P / 50
+	D := 0.01
+
+	pitchSetpoint := 1.0
+	pitch := pid.Controller{
+		Config: pid.ControllerConfig{
+			ProportionalGain: P,
+			IntegralGain:     I,
+			DerivativeGain:   D,
+		},
+	}
+
+	rollSetpoint := 0.0
+	roll := pid.Controller{
+		Config: pid.ControllerConfig{
+			ProportionalGain: P,
+			IntegralGain:     I,
+			DerivativeGain:   D,
+		},
+	}
+
+	for {
+		select {
+		case <-ticker.C:
+			posi, err := xpc.GetPOSI(conn, 0)
+			if err != nil {
+				panic(err)
+			}
+
+			pitch.Update(pid.ControllerInput{
+				ReferenceSignal:  pitchSetpoint,
+				ActualSignal:     float64(posi.Pitch),
+				SamplingInterval: samplingInterval,
+			})
+
+			roll.Update(pid.ControllerInput{
+				ReferenceSignal:  rollSetpoint,
+				ActualSignal:     float64(posi.Roll),
+				SamplingInterval: samplingInterval,
+			})
+
+			if err := xpc.SendCTRL(conn, &xpc.CTRL{
+				Elevator:   float32(pitch.State.ControlSignal),
+				Aileron:    float32(roll.State.ControlSignal),
+				Rudder:     -998,
+				Throttle:   -998,
+				Flaps:      -998,
+				Speedbrake: -998,
+			}); err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("PITCH: %.2f -> %.2f	ROLL: %.2f -> %.2f\n",
+				posi.Pitch, pitch.State.ControlSignal, posi.Roll, roll.State.ControlSignal)
+		}
+	}
+}
---
+++ Go/examples/autopilot/go.mod
@@ -0,0 +1,12 @@
+module autopilot
+
+go 1.23.0
+
+replace xpc => ../../xpc
+
+require (
+	go.einride.tech/pid v0.1.3
+	xpc v0.0.0-00010101000000-000000000000
+)
+
+require github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40 // indirect
